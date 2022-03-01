7a1ba537f74a7546e98c27528ca9c5c8ef263d8d
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-03-01 08:53:41 +0300

    Done

--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,22 @@ function changeState(state, action) {
   }
 }
 
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
 changeState(state, action);
+
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
+
+render();
+
+dispatch({ type: "counter/increment" });
+dispatch({ type: "counter/increment" });
